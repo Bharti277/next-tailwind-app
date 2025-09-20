c52dda672b4874538c503be0042d0ad2e47a955b
Bharti277 <bhartideependra277@gmail.com>
2025-09-20 21:34:21 +0530

    wip

--- app/(users)/blog/[...slug]/page.js
+++ app/(users)/blog/[...slug]/page.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Blog = async (props) => {
   const { slug } = await props.params;
-  console.log(slug);
+  console.log(slug, "slug");
   return <div>Blog</div>;
 };
 
