938fb26dfa436c0cede8a1d6d14120febca8adaf
Bharti277 <bhartideependra277@gmail.com>
2025-09-22 19:50:59 +0530

    alignment

--- app/not-found.js
+++ app/not-found.js
@@ -6,7 +6,7 @@ import React from "react";
 const NotFound = () => {
   const router = useRouter();
   return (
-    <div>
+    <div className="text-center m-auto">
       <h1>404 - Page Not Found</h1>
       <button onClick={() => router.push("/")}>Go to Home</button>
       <button onClick={() => router.back()}>Go Back</button>
